d2724439393d2f132997f411199338dd78902069
Hongsugyeong <olivia258@naver.com>
2022-07-22 09:29:39 +0900

    Input, Output 추가

---
+++ src/com/gang/io/IOMain1.java
@@ -0,0 +1,26 @@
+package com.gang.io;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class IOMain1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		InputStream is = System.in;
+		InputStreamReader ir = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(ir);
+		try {
+			System.out.println("키보드에서 입력 후 엔터");
+			String msg = br.readLine();
+			System.out.println(msg);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
