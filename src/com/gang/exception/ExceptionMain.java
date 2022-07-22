5184a445e30e21aab15b18a2d1a42c75b83f7e40
Hongsugyeong <olivia258@naver.com>
2022-07-22 12:36:49 +0900

    예외 추가

--- src/com/gang/exception/ExceptionMain.java
+++ src/com/gang/exception/ExceptionMain.java
@@ -3,8 +3,11 @@ package com.gang.exception;
 public class ExceptionMain {
 
 	public static void main(String[] args) {
+		System.out.println("메인 시작");
 		Test test = new Test();
-		test.t1();
+		//test.t1();
+		System.out.println("메인 종료");
+		test.t2();
 
 		
 	}
--- src/com/gang/exception/Test.java
+++ src/com/gang/exception/Test.java
@@ -1,11 +1,55 @@
 package com.gang.exception;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Test {
 	
+	public void t3() {
+		
+	}
+	
+	public void t2() {
+		String [] str = {"10", "five", "3.14","0"};
+		Scanner sc = new Scanner(System.in);
+		
+		
+		for(String s:str) {
+			try {
+				System.out.println("숫자를 입력하세요");
+				
+				int n = sc.nextInt();
+			int num = Integer.parseInt(s);
+			num = n/num;
+			System.out.println(num);
+			}catch(NumberFormatException e) {
+				System.out.println("예외처리");		
+			}catch(ArithmeticException e) {
+				System.out.println("수학 오류 해결");
+			}catch(Throwable e) {
+				
+			}
+		}
+	}
+	
 	public void t1() {
+		Random random = new Random();
 		String str = null;
 		
+		if(random.nextInt(10)%2 == 0) {
+			str = "10";
+		}
+		int num = Integer.parseInt("10"); // New NumberException
+		
+		try {
+		num = num / 0;
+		System.out.println(num);
+		}catch(ArithmeticException e) {
+			System.out.println("예외처리");
+			
+		}
 		System.out.println(str.toString());
+		new NullPointerException();
 	}
 
 }
