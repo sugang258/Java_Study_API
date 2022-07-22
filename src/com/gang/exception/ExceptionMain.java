4324e4c390ae62a33eab0f5151ad2b43e85619d6
Hongsugyeong <olivia258@naver.com>
2022-07-22 09:29:27 +0900

    예외 추가

---
+++ src/com/gang/exception/ExceptionMain.java
@@ -0,0 +1,12 @@
+package com.gang.exception;
+
+public class ExceptionMain {
+
+	public static void main(String[] args) {
+		Test test = new Test();
+		test.t1();
+
+		
+	}
+
+}
---
+++ src/com/gang/exception/Test.java
@@ -0,0 +1,11 @@
+package com.gang.exception;
+
+public class Test {
+	
+	public void t1() {
+		String str = null;
+		
+		System.out.println(str.toString());
+	}
+
+}
