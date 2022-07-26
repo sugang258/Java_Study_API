aba300ca14a477d8bebe903ffa5be0c00fbece43
Hongsugyeong <olivia258@naver.com>
2022-07-26 09:35:37 +0900

    StudentDAO, StudentService 수정

--- src/com/gang/io/file/ex1/StudentDAO.java
+++ src/com/gang/io/file/ex1/StudentDAO.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -23,16 +24,41 @@ public class StudentDAO {
 		//작성 형식은 기존 내용의 형식과 동일하게
 		//1을 리턴 : 정상적인 성공
 		//0을 리턴 : 예외가 발생했을경우
+		int result = 1;
+		File file = new File("C:\\study", "studentdata.txt");
+		FileWriter fw = null;
+		try {
+			fw = new FileWriter(file);
+			fw.write("\r\n");
+			for(StudentDTO studentDTO : ar) {
+				StringBuffer stringBuffer = new StringBuffer();
+				stringBuffer.append(studentDTO.getName() + ",");
+				stringBuffer.append(studentDTO.getNum() + ",");
+				stringBuffer.append(studentDTO.getKor() + ",");
+				stringBuffer.append(studentDTO.getKor() + ",");
+				stringBuffer.append(studentDTO.getEng() + ",");
+				stringBuffer.append(studentDTO.getMath() + "\r\n");
+				
+				fw.write(stringBuffer.toString());
+
+			}				
+
+			fw.flush();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			result = 0;
+		}
 		
 		
-		return 0;
+		return result;
 		
 	}
 	
 	//1. getList
 	//	  studentdata 파일의 내용을 읽어서 작업
 	
-	public void getList() throws IOException {
+	public ArrayList<StudentDTO> getList() throws Exception {
 		File file = new File("C:\\study\\studentdata.txt");
 		FileReader fr = new FileReader(file);
 		BufferedReader br = new BufferedReader(fr);
@@ -40,6 +66,7 @@ public class StudentDAO {
 		ArrayList<StudentDTO> arraylist = new ArrayList<StudentDTO>();
 		
 		boolean check = true;
+		br.readLine();
 		while(check) {
 			String text = br.readLine();
 			
@@ -57,7 +84,7 @@ public class StudentDAO {
 			studentDTO.setAvg(studentDTO.getTotal()/3);
 			arraylist.add(studentDTO);
 			
-			
+			/*
 			System.out.println("이름 : " + studentDTO.getName());
 			System.out.println("번호 : " + studentDTO.getNum());
 			System.out.println("국어점수 : " + studentDTO.getKor());
@@ -66,8 +93,9 @@ public class StudentDAO {
 			System.out.println("총점 : " + studentDTO.getTotal());
 			System.out.println("평균 : " + studentDTO.getAvg());
 			System.out.println("================");
-					
+			*/
 		}
+		return arraylist;
 	}
 
 }
--- src/com/gang/io/file/ex1/StudentService.java
+++ src/com/gang/io/file/ex1/StudentService.java
@@ -1,5 +1,91 @@
 package com.gang.io.file.ex1;
 
-public class StudentService {
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class StudentService implements Service {
+	private StudentDAO stdao;
+	Scanner sc;
+	
+	public StudentService() {
+		this.stdao = new StudentDAO();
+		this.sc = new Scanner(System.in);
+	}
+
+	@Override
+	public ArrayList<StudentDTO> getList() throws Exception {
+		
+		ArrayList<StudentDTO> ar = stdao.getList();
+		for(StudentDTO studentDTO:ar) {
+			studentDTO.setTotal(studentDTO.getKor() + studentDTO.getEng() + studentDTO.getMath());
+			studentDTO.setAvg(studentDTO.getTotal() / 3);
+		}
+		
+		
+		return ar;
+		
+	}
+	
+	@Override
+	public int setList(ArrayList<StudentDTO> ar) {
+		
+		
+		return stdao.setList(ar);
+		
+	}
+	
+	@Override
+	public StudentDTO getStudent(ArrayList<StudentDTO> ar) {
+		StudentDTO studentDTO = null;
+		System.out.println("검색할 학생의 번호를 입력하세요");
+		int st = sc.nextInt();
+		
+		for(StudentDTO studentDTO2 : ar) {
+			if(st == studentDTO2.getNum()) {
+				studentDTO = studentDTO2;
+				break;
+			}
+		}
+		
+		return studentDTO;
+		
+	}
+	
+	@Override
+	public int setStudentDelete(ArrayList<StudentDTO> ar) {
+		int result = 0;
+		System.out.println("삭제할 학생 번호 입력");
+		int num = sc.nextInt();
+		for(int i=0;i<ar.size();i++) {
+			if(num == ar.get(i).getNum()) {
+				StudentDTO st = ar.remove(i);
+				if(st != null) {
+					result = 1;
+				}
+			}
+		}
+		return result;
+		
+	}
+	
+	@Override
+	public void setStudentAdd(ArrayList<StudentDTO> ar) {
+		StudentDTO studentDTO = new StudentDTO();
+		System.out.println("추가할 학생 이름 입력");
+		studentDTO.setName(sc.next());
+		System.out.println("번호 입력");
+		studentDTO.setNum(sc.nextInt());
+		System.out.println("국어 입력");
+		studentDTO.setKor(sc.nextInt());
+		System.out.println("영어 입력");
+		studentDTO.setEng(sc.nextInt());
+		System.out.println("수학 입력");
+		studentDTO.setMath(sc.nextInt());
+		studentDTO.setTotal(studentDTO.getKor()+studentDTO.getEng()+studentDTO.getMath());
+		studentDTO.setAvg(studentDTO.getTotal()/3);
+		
+		
+	}
 
 }
